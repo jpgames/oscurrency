0b5073be2ff666188e11dc2add2180b96073a19d
Tom Brown <herestomwiththeweather@gmail.com>
2025-01-05 14:49:47 -0600

    fix application helper specs. closes gh-64

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -6,25 +6,25 @@ describe ApplicationHelper do
 
     it "leaves short sentences untouched" do
       sentence = "a b  c   d"
-      helper.first_n_words(sentence).should == sentence
+      expect(helper.first_n_words(sentence)).to eq(sentence)
     end
 
     it "shortens long sentences" do
       sentence = "a b  c   d " * 8
       sentence.strip!
       shortened = helper.first_n_words(sentence)
-      shortened.should_not == sentence
-      shortened.split.should have(20).words
-      sentence.should start_with(shortened)
+      expect(shortened).to_not eq(sentence)
+      expect(shortened.split.size).to eq(20)
+      expect(sentence).to start_with(shortened)
     end
 
     it "shortens long sentences (specified length)" do
       sentence = "a b  c   d " * 3
       sentence.strip!
       shortened = helper.first_n_words(sentence, 8)
-      shortened.should_not == sentence
-      shortened.split.should have(8).words
-      sentence.should start_with(shortened)
+      expect(shortened).to_not eq(sentence)
+      expect(shortened.split.size).to eq(8)
+      expect(sentence).to start_with(shortened)
     end
 
   end
